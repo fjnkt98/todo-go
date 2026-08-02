29a8b9035dd79982647bca59c3c2e0bab6d31698
fjnkt98 <fjnkt98@gmail.com>
2026-08-02 17:32:33 +0900

    migrate into pure sqlite3 and sqldef

--- go.mod
+++ go.mod
@@ -2,10 +2,4 @@ module github.com/fjnkt98/todo-go
 
 go 1.26.5
 
-require github.com/tursodatabase/go-libsql v0.0.0-20260424063416-3051e37e6e04
-
-require (
-	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
-	github.com/libsql/sqlite-antlr4-parser v0.0.0-20240327125255-dbf53b6cbf06 // indirect
-	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
-)
+require github.com/mattn/go-sqlite3 v1.14.49
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log/slog"
 	"os"
 
-	_ "github.com/tursodatabase/go-libsql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -15,16 +15,29 @@ func main() {
 
 	ctx := context.Background()
 
-	db, err := sql.Open("libsql", os.Getenv("DATABASE_URL"))
+	db, err := sql.Open("sqlite3", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		slog.LogAttrs(ctx, slog.LevelError, "open database", slog.Any("error", err))
 		return
 	}
 	defer db.Close() //nolint: errcheck
 
+	if _, err := db.ExecContext(ctx, "PRAGMA journal_mode = WAL;"); err != nil {
+		slog.LogAttrs(ctx, slog.LevelError, "pragma journal mode", slog.Any("error", err))
+		return
+	}
+	if _, err := db.ExecContext(ctx, "PRAGMA foreign_keys = on;"); err != nil {
+		slog.LogAttrs(ctx, slog.LevelError, "pragma foreign keys", slog.Any("error", err))
+		return
+	}
+	if _, err := db.ExecContext(ctx, "PRAGMA defer_foreign_keys = on;"); err != nil {
+		slog.LogAttrs(ctx, slog.LevelError, "pragma defer foreign keys", slog.Any("error", err))
+		return
+	}
+
 	slog.LogAttrs(ctx, slog.LevelInfo, "todo-go")
 
-	rows, err := db.QueryContext(ctx, "SELECT 1;")
+	rows, err := db.QueryContext(ctx, "SELECT id, name FROM users;")
 	if err != nil {
 		slog.LogAttrs(ctx, slog.LevelError, "fetch rows", slog.Any("error", err))
 		return
@@ -32,12 +45,13 @@ func main() {
 	defer rows.Close() //nolint: errcheck
 
 	for rows.Next() {
-		var num int
-		if err := rows.Scan(&num); err != nil {
+		var id int
+		var name string
+		if err := rows.Scan(&id, &name); err != nil {
 			slog.LogAttrs(ctx, slog.LevelError, "fetch rows", slog.Any("error", err))
 			return
 		}
-		slog.LogAttrs(ctx, slog.LevelInfo, "row fetched", slog.Int("value", num))
+		slog.LogAttrs(ctx, slog.LevelInfo, "row fetched", slog.Int("id", id), slog.String("name", name))
 	}
 
 	if err := rows.Err(); err != nil {
